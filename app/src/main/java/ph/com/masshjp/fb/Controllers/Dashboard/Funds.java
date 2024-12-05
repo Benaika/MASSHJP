053245f7318e22b7bf56fcbe5a4083bbffde1e2d
Benaika <71858253+Benaika@users.noreply.github.com>
2024-12-06 00:15:14 +0800

    profile ui updated

--- app/src/main/java/ph/com/masshjp/fb/Controllers/Dashboard/Funds.java
+++ app/src/main/java/ph/com/masshjp/fb/Controllers/Dashboard/Funds.java
@@ -8,19 +8,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import ph.com.masshjp.fb.BaseActivity;
 import ph.com.masshjp.fb.R;
 
-public class Funds extends AppCompatActivity {
+public class Funds extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_funds);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //setToolbar(this, R.id.toolbar, "Funds");
     }
 }
