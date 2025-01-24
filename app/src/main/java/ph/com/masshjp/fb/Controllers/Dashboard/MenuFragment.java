75478c2038c2df394f2af3b1e81dc9ea9ad9a75d
Benaika <71858253+Benaika@users.noreply.github.com>
2025-01-24 22:21:33 +0800

    Menu Fragment Remove
    Removing some portions on menu fragment

--- app/src/main/java/ph/com/masshjp/fb/Controllers/Dashboard/MenuFragment.java
+++ app/src/main/java/ph/com/masshjp/fb/Controllers/Dashboard/MenuFragment.java
@@ -33,62 +33,16 @@ import de.hdodenhof.circleimageview.CircleImageView;
 import ph.com.masshjp.fb.Controllers.Login.LoginActivity;
 import ph.com.masshjp.fb.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MenuFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MenuFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     //Firebase
     private CircleImageView profileImage;
     private FirebaseAuth auth;
     private FirebaseFirestore fStore;
 
-    WebView webView;
     ProgressDialog progressDialog;
     CardView cv_profile, cv_masterlist, cv_attendance, cv_scheduling, cv_funds, cv_liveMass, cv_mpage, cv_website, cv_logout;
     TextView tv_fullname;
-
-    public MenuFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MenuFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MenuFragment newInstance(String param1, String param2) {
-        MenuFragment fragment = new MenuFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
